4b1f3c3cbbd323916922f2473b65345d1992d7db
Nazar Dolnyy <nazar.dolnyy@implex.dev>
2022-08-01 23:34:07 +0300

    Fixed navigation. Added animations to blocks

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,12 +3,63 @@
  * All rights reserved
  */
 
-const nav = document.querySelector('nav');
+const implex = (function () {
+  const START_TRANSITION_CLASS = 'start';
+  const HEADER_ID = 'header';
 
-window.onscroll = () => {
-    if (document.documentElement.scrollTop > 100) {
-        nav.classList.add('black-background');
-    } else {
-        nav.classList.remove('black-background');
+  let sections;
+  let sectionsToshow;
+  let header;
+
+  document.addEventListener("DOMContentLoaded", init);
+
+
+  function init() {
+    sections = document.getElementsByTagName('section');
+    sectionsToshow = sections.length;
+    header = document.getElementById(HEADER_ID);
+
+
+    setTimeout(() => {  
+      checkSectionPositions();
+    }, 0);
+
+    window.addEventListener('scroll', checkSectionPositions);
+  }
+
+  function checkSectionPositions() {
+    const innerHeight = (window.innerHeight || document.documentElement.clientHeight) * 0.8;
+
+    Array.prototype.forEach.call(sections, section => {
+      if (
+        isElementInViewport(section, innerHeight) &&
+        !section.classList.contains(START_TRANSITION_CLASS)
+      ) {
+        section.classList.add(START_TRANSITION_CLASS);
+        sectionsToshow--;
+      }
+    });
+
+    if (!sectionsToshow) {
+      window.removeEventListener('scroll', checkSectionPositions);
     }
-};
+  }
+
+  function isElementInViewport(element, viewportHeight) {
+    const rect = element.getBoundingClientRect();
+
+    return rect.top <= viewportHeight;
+  }
+
+  window.onscroll = () => {
+    const additionalClassName = 'black-opaque';
+
+    if (document.documentElement.scrollTop > 100) {
+      header.classList.add(additionalClassName);
+    } else {
+      header.classList.remove(additionalClassName);
+    }
+  };
+
+  return {};
+})();
