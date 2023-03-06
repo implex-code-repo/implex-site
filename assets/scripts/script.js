497a731959ca932f70f972ba88abd59249d94d06
implex-code-repo <113038869+implex-code-repo@users.noreply.github.com>
2023-03-06 11:27:09 +0000

    Hide Clutch iframe until load finished

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -20,6 +20,11 @@ const implex = (function () {
   document.addEventListener("DOMContentLoaded", init);
 
   function init() {
+    // Get the iframe element by ID
+    const iframe = document.getElementById('iframe-0.9792492733831788');
+    // Remove the "hidden" attribute
+    iframe.removeAttribute('hidden');
+    
     if (document.getElementsByClassName('carousel').length !== 0) {
       const splide = new Splide('.splide', {
         drag: isTouchScreen(),
